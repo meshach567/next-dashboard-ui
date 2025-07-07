901631626c66e5d14796f432e00b1166242b2e33
Meshach Arinze <arinzemeshach123@gmail.com>
2025-07-07 13:59:01 +0100

    refactor: modify the DashboardLayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import DashboardLayout from "./(dashboard)/layout"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className=''>
+      <DashboardLayout>
+        <div className="flex items-center justify-center h-full">
+          <h1 className="text-2xl font-bold">Welcome to SchooLama!</h1>
+        </div>
+      </DashboardLayout>
+    </div>
   )
 }
 
